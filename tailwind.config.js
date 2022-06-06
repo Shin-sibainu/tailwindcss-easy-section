1ac1ccb18129ba79a890d9e68f334383b568ebb3
Shin-sibainu <shincode0712@gmail.com>
2022-06-06 18:59:41 +0900

    done

---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {
+      colors: {
+        primary: {
+          100: "#ebf8ff",
+          300: "#90cdf4",
+          500: "#4299e1",
+          700: "#2b6cb0",
+          900: "#2a4365",
+        },
+      },
+    },
+  },
+  plugins: [],
+};
